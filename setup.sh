44e787df03701a728598bc29fa55ecc50fa7a127
LarissaCasteluci <lari.casteluci@gmail.com>
2021-11-15 12:04:58 -0300

    First version of setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#Make all Directories for the projects
+mkdir GGCNN
+mkdir DexNet2
+mkdir DenseObjectNet
+
+#Download GG-CNN 
+cd GGCNN
+git clone https://github.com/dougsm/ggcnn.git
+
+#Download DexNet Family
+cd ../DexNet2
+git clone https://github.com/BerkeleyAutomation/gqcnn
+
+#Download DenseObjectNet
+cd ../DenseObjectNet
+git clone https://github.com/RobotLocomotion/pytorch-dense-correspondence

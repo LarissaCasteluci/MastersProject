24326702b5c28b3d98bda3b5e702c6768f90b320
Larissa <lari.casteluci@gmail.com>
2022-08-30 22:19:48 -0300

    reestructuring jacquard data exporter

--- 1DatasetGeneration/src/data_exporters/jacquard_format.py
+++ 1DatasetGeneration/src/data_exporters/jacquard_format.py
@@ -1,4 +1,5 @@
 from base_data_exporter import BaseDataExporter
+import numpy as np
 from dataclasses import dataclass
 
 pixels = float
@@ -15,11 +16,37 @@ class GraspAnnotation:
     jaws_size: centimeters
 
 
+@dataclass
+class ObjectSample:
+    index: int
+    grasps_list: list[GraspAnnotation]
+    perfect_depth: np.array
+    rgb: np.array
+    stereo_depth: np.array
+
+
+@dataclass
+class ObjectData:
+    object_id: int
+    object_data: list[ObjectSample]
+
+
 class JacquardDataExporter(BaseDataExporter):
-    annotations_list: list[GraspAnnotation]
+    object_id: int
+    annotations_list: list[ObjectData]
     save_path: str
     n_samples_per_object: int
 
-    def __init__(self, n_samples_per_object=5):
+    def __init__(self, save_path: str, n_samples_per_object: int = 5):
+        self.save_path = save_path
         self.n_samples_per_object = n_samples_per_object
+        self.annotations_list = []
+
+    def add_new_object(self):
+        pass
+
+    def save(self):
+        pass
+
+
 

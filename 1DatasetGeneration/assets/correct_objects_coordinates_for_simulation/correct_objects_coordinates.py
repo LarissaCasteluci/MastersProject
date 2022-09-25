a4c0761b25c2fb145e54d2a384ee0c35762036af
LarissaCasteluci <lari.casteluci@gmail.com>
2022-09-25 12:44:38 +0000

    fixing

--- 1DatasetGeneration/assets/correct_objects_coordinates_for_simulation/correct_objects_coordinates.py
+++ 1DatasetGeneration/assets/correct_objects_coordinates_for_simulation/correct_objects_coordinates.py
@@ -1,4 +1,5 @@
 """
+#   https://towardsdatascience.com/how-to-voxelize-meshes-and-point-clouds-in-python-ca94d403f81d
     Correct Wavefront object format ( .obj ) for simulation
     1. Decrease size ( Make biggest size == 1 )
     2. Voxelize
@@ -14,12 +15,19 @@ from pathlib import Path
 import re
 import numpy as np
 import trimesh
+import pdb
 
-path = Path("/home/larissa/MastersProject/original_repos/egadevalset/egad_eval_set")
-file = "A1.obj"
+#path = Path("/home/larissa/MastersProject/original_repos/egadevalset/egad_eval_set")
+#file = "A1.obj"
+
+path = Path("/workspaces/MastersProject/1DatasetGeneration/assets/grasp_objects")
+obj_name = 'A0'
+file = f"{obj_name}_visual.obj"
+file_tmp_1 = f"{obj_name}_tmp1.obj"
 
 coords = []
 
+# Load file
 with open(str(path / file), 'r') as f:
     obj = f.readlines()
 
@@ -35,6 +43,24 @@ coords = np.asarray(coords)
 x, y, z = coords[0:3]
 norm = max([(np.max(x) - np.min(x)), (np.max(y) - np.min(y)), (np.max(z) - np.min(z))])
 coords = coords / norm
-print(coords)
 
-mesh = trimesh.l
+# Save normalized file
+with open(str(path.parent / 'correct_objects_coordinates_for_simulation'/ 'tmp'/ file_tmp_1), 'w') as f:
+    for xyz in coords:
+        f.write(f"v {xyz[0]} {xyz[1]} {xyz[2]}\n")
+    
+    for line in obj:
+        r = re.search(r"(?:f)(.+)", line)
+        if r:
+            f.write(line)
+
+# Voxelize
+mesh = trimesh.load(str(path.parent / 'correct_objects_coordinates_for_simulation'/ 'tmp'/ file_tmp_1))
+angel_voxel = mesh.voxelized(0.01)
+
+pdb.set_trace()
+
+print(angel_voxel.points) # Verify if it is ok
+
+
+
